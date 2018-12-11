c09c3bca882b1c8667a2d15d07433b534038a5e2
vaibhav28398 <31373963+vaibhav28398@users.noreply.github.com>
2018-12-11 14:37:16 +0530

    Add files via upload

--- spandan_music.sql
+++ spandan_music.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 17, 2018 at 03:15 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.10
+-- Generation Time: Dec 11, 2018 at 09:59 AM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,6 +36,56 @@ CREATE TABLE `album_details` (
   `album_art` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `album_details`
+--
+
+INSERT INTO `album_details` (`tp_id`, `uid`, `album_id`, `album_name`, `album_art`) VALUES
+(1, 3, 1, 'Happy', '2015Coldplay_AHeadFullOfDreams271115-1.jpg'),
+(2, 3, 2, 'Awesome', 'imaginedragons-0.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `msg_id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `message` varchar(1000) NOT NULL,
+  `msg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`msg_id`, `sender_id`, `message`, `msg_time`) VALUES
+(1, 3, 'kp', '2018-09-26 20:50:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `queue_details`
+--
+
+CREATE TABLE `queue_details` (
+  `tp_id` int(11) NOT NULL,
+  `queue_id` int(11) NOT NULL,
+  `song_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `queue_details`
+--
+
+INSERT INTO `queue_details` (`tp_id`, `queue_id`, `song_id`) VALUES
+(4, 3, 4),
+(5, 3, 0),
+(6, 3, 10),
+(7, 3, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +100,13 @@ CREATE TABLE `song_details` (
   `hits` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `song_details`
+--
+
+INSERT INTO `song_details` (`song_id`, `song_name`, `artist`, `genre`, `hits`) VALUES
+(2, 'Traceback most recent call last  File hello.py line 1 in module    from mp3tagger import MP3File VER', 'Traceback most recent call last  File hello.py line 1 in module    from mp3tagger import MP3File VER', 'traceback most recent call last  file hello.py line 1 in module    from mp3tagger import mp3file ver', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -62,9 +119,23 @@ CREATE TABLE `user_details` (
   `email` varchar(100) NOT NULL,
   `pass` varchar(100) NOT NULL,
   `verify_key` varchar(100) NOT NULL,
+  `reset_token` varchar(100) DEFAULT NULL,
+  `reset_complete` varchar(100) NOT NULL,
   `reg_date` date NOT NULL,
-  `last_login` datetime NOT NULL
+  `last_login` datetime NOT NULL,
+  `msg_notification` int(11) NOT NULL DEFAULT '0',
+  `has_queue` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_details`
+--
+
+INSERT INTO `user_details` (`uid`, `name`, `email`, `pass`, `verify_key`, `reset_token`, `reset_complete`, `reg_date`, `last_login`, `msg_notification`, `has_queue`) VALUES
+(1, 'Ankit Bhadage', 'abhadage@gmail.com', 'e0e289f71a4e7e3de4f09208fa55836a', 'Yes', NULL, 'Yes', '2018-09-25', '2018-09-25 13:41:16', 1, 0),
+(2, 'Venkatesh Deore', 'deore.venkatesh@gmail.com', 'e4637652573268f24fd5161422f2e5dc', 'Yes', NULL, 'No', '2018-09-25', '2018-09-25 14:13:17', 1, 0),
+(3, 'Vaibhav Raizu', 'vaibhav.raizada.mnnit@gmail.com', '28c771216b8d5d65aa97226ca8629f5a', 'Yes', NULL, 'No', '2018-09-25', '2018-09-29 23:08:58', 0, 0),
+(4, 'Shahabaz Khan', 'shaz12cuteboy@gmail.com', 'eabcc33cd4445e424ddfdc1ca7f9a445', 'Yes', NULL, 'No', '2018-09-25', '2018-09-25 00:59:54', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -77,8 +148,17 @@ CREATE TABLE `user_song_map` (
   `uid` int(11) NOT NULL,
   `song_id` int(11) NOT NULL,
   `custom_name` varchar(100) NOT NULL,
-  `album_id` int(11) NOT NULL
+  `album_id` int(11) NOT NULL,
+  `is_private` int(11) NOT NULL DEFAULT '0',
+  `last_paused` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_song_map`
+--
+
+INSERT INTO `user_song_map` (`tp_id`, `uid`, `song_id`, `custom_name`, `album_id`, `is_private`, `last_paused`) VALUES
+(2, 3, 2, 'Love', 2, 1, 20);
 
 --
 -- Indexes for dumped tables
@@ -88,6 +168,18 @@ CREATE TABLE `user_song_map` (
 -- Indexes for table `album_details`
 --
 ALTER TABLE `album_details`
+  ADD PRIMARY KEY (`tp_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`msg_id`);
+
+--
+-- Indexes for table `queue_details`
+--
+ALTER TABLE `queue_details`
   ADD PRIMARY KEY (`tp_id`);
 
 --
@@ -116,25 +208,37 @@ ALTER TABLE `user_song_map`
 -- AUTO_INCREMENT for table `album_details`
 --
 ALTER TABLE `album_details`
-  MODIFY `tp_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `tp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `queue_details`
+--
+ALTER TABLE `queue_details`
+  MODIFY `tp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `song_details`
 --
 ALTER TABLE `song_details`
-  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `song_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user_details`
 --
 ALTER TABLE `user_details`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user_song_map`
 --
 ALTER TABLE `user_song_map`
-  MODIFY `tp_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `tp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
